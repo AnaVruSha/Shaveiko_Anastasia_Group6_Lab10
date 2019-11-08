6f8b1f64169744dbae92e869298b3b5497cea3c6
Anastasiya shaveyko <anavrusha@mail.ru>
2019-11-08 10:14:29 +0300

    Lab 10 ITechArt

---
+++ Shaveiko_Anastasia_Group6_Lab10/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shaveiko_Anastasia_Group6_Lab10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shaveiko_Anastasia_Group6_Lab10")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("167222f9-54aa-400f-978d-929673735d35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Shaveiko_Anastasia_Group6_Lab10/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Shaveiko_Anastasia_Group6_Lab10
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ChromeDriver chromeDriver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("http://www.google.by/");
+            chromeDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).SendKeys("iTechArt");
+            chromeDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).SendKeys(Keys.Enter);
+            Assert.IsTrue(chromeDriver.FindElement(By.XPath("//*[@id=\"rso\"]/div[1]")).Text.Contains("iTechArt"));
+            Assert.IsTrue(chromeDriver.Title.Contains("iTechArt"));
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            chromeDriver.Quit();
+        }
+    }
+}
